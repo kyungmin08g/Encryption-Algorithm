3ebae928c8e631563bc3f625a0abeb30237c3cfa
Kim KyungMin <k.kyungmin2892@gmail.com>
2025-05-24 01:23:37 +0900

    feat: AES Converter 생성

---
+++ src/main/java/kyungmin/encryptionalgorithm/config/jpa/AESCryptoConverter.java
@@ -0,0 +1,26 @@
+package kyungmin.encryptionalgorithm.config.jpa;
+
+import jakarta.persistence.AttributeConverter;
+import jakarta.persistence.Converter;
+import kyungmin.encryptionalgorithm.symmetric.block.AES.AESCryptoUtils;
+import org.springframework.util.StringUtils;
+
+@Converter
+public class AESCryptoConverter implements AttributeConverter<String, String> {
+
+  @Override
+  public String convertToDatabaseColumn(String text) {
+    if (!StringUtils.hasText(text)) {
+      return null;
+    }
+    return AESCryptoUtils.encrypt(text);
+  }
+
+  @Override
+  public String convertToEntityAttribute(String dbData) {
+    if (!StringUtils.hasText(dbData)) {
+      return null;
+    }
+    return AESCryptoUtils.encrypt(dbData);
+  }
+}
