38ab29e79caa8951f829edb0edec856f1ba2b98c
Kim KyungMin <k.kyungmin2892@gmail.com>
2025-05-23 23:04:32 +0900

    chore: 프로젝트 세팅

---
+++ src/main/java/kyungmin/encryptionalgorithm/EncryptionAlgorithmApplication.java
@@ -0,0 +1,11 @@
+package kyungmin.encryptionalgorithm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EncryptionAlgorithmApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(EncryptionAlgorithmApplication.class, args);
+  }
+}
---
+++ src/test/java/kyungmin/encryptionalgorithm/EncryptionAlgorithmApplicationTests.java
@@ -0,0 +1,13 @@
+package kyungmin.encryptionalgorithm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EncryptionAlgorithmApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
