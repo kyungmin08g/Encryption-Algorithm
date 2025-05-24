a7a19164cf8024f0197164a628a28c29f0e785b8
Kim KyungMin <k.kyungmin2892@gmail.com>
2025-05-24 11:15:32 +0900

    feat: 더미 데이터 생성을 위한 서비스 구현

--- src/main/java/kyungmin/encryptionalgorithm/symmetric/block/DES/controller/DESController.java
+++ src/main/java/kyungmin/encryptionalgorithm/symmetric/block/DES/controller/DESController.java
@@ -2,8 +2,8 @@ package kyungmin.encryptionalgorithm.symmetric.block.DES.controller;
 
 import kyungmin.encryptionalgorithm.symmetric.block.DES.service.DESService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -11,5 +11,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class DESController {
   private final DESService service;
 
+  @PostMapping(value = "/create")
+  public ResponseEntity<Void> create(@RequestParam(name = "phone") String phoneNumber) {
+    service.create(phoneNumber);
+    return ResponseEntity.ok().build();
+  }
 
+  @GetMapping(value = "")
+  public ResponseEntity<String> getPhoneNumber(@RequestParam(name = "id") String id) {
+    return ResponseEntity.ok(service.getPhoneNumber(id));
+  }
 }
--- src/main/java/kyungmin/encryptionalgorithm/symmetric/block/DES/service/DESService.java
+++ src/main/java/kyungmin/encryptionalgorithm/symmetric/block/DES/service/DESService.java
@@ -1,5 +1,6 @@
 package kyungmin.encryptionalgorithm.symmetric.block.DES.service;
 
+import kyungmin.encryptionalgorithm.symmetric.block.DES.domain.DES;
 import kyungmin.encryptionalgorithm.symmetric.block.DES.repository.DESRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -9,5 +10,16 @@ import org.springframework.stereotype.Service;
 public class DESService {
   private final DESRepository repository;
 
+  public void create(String phoneNumber) {
+    repository.save(
+      DES.builder()
+        .phoneNumber(phoneNumber)
+        .build()
+    );
+  }
 
+  public String getPhoneNumber(String id) {
+    return repository.findById(Long.parseLong(id))
+      .get().getPhoneNumber();
+  }
 }
