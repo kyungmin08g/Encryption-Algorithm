6a066f256e4181e82bebab7d9c80b4e892873f15
Kim KyungMin <k.kyungmin2892@gmail.com>
2025-05-24 17:59:36 +0900

    feat: Blowfish 알고리즘을 테스트할 클래스 구현

--- src/main/java/kyungmin/encryptionalgorithm/symmetric/block/Blowfish/controller/BlowfishController.java
+++ src/main/java/kyungmin/encryptionalgorithm/symmetric/block/Blowfish/controller/BlowfishController.java
@@ -2,8 +2,8 @@ package kyungmin.encryptionalgorithm.symmetric.block.Blowfish.controller;
 
 import kyungmin.encryptionalgorithm.symmetric.block.Blowfish.service.BlowfishService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -11,4 +11,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class BlowfishController {
   private final BlowfishService service;
 
+  @PostMapping(value = "/create")
+  public ResponseEntity<Void> create(@RequestParam(name = "phone") String phoneNumber) {
+    service.create(phoneNumber);
+    return ResponseEntity.ok().build();
+  }
+
+  @GetMapping(value = "/get")
+  public ResponseEntity<String> getPhoneNumber(@RequestParam(name = "id") String id) {
+    return ResponseEntity.ok(service.getPhoneNumber(id));
+  }
 }
--- src/main/java/kyungmin/encryptionalgorithm/symmetric/block/Blowfish/service/BlowfishService.java
+++ src/main/java/kyungmin/encryptionalgorithm/symmetric/block/Blowfish/service/BlowfishService.java
@@ -1,5 +1,6 @@
 package kyungmin.encryptionalgorithm.symmetric.block.Blowfish.service;
 
+import kyungmin.encryptionalgorithm.symmetric.block.Blowfish.domain.Blowfish;
 import kyungmin.encryptionalgorithm.symmetric.block.Blowfish.repository.BlowfishRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -9,4 +10,16 @@ import org.springframework.stereotype.Service;
 public class BlowfishService {
   private final BlowfishRepository repository;
 
+  public void create(String phoneNumber) {
+    repository.save(
+      Blowfish.builder()
+        .phoneNumber(phoneNumber)
+        .build()
+    );
+  }
+
+  public String getPhoneNumber(String id) {
+    return repository.findById(Long.parseLong(id))
+      .get().getPhoneNumber();
+  }
 }
